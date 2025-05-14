ff0447c6dbc4095719eda876c32e48ea6b9a6b2c
Seth Gorelik <srgorelik@users.noreply.github.com>
2025-05-14 11:11:22 -0500

    final test of automation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <h1>Hello Madison</h1>
+            <h1>Automation Test</h1>
         </div>
     );
 }
